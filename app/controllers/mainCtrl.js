620ca770f88036dfdaab1914d82de5c469c73856
Makoy <neimarkbraga@gmail.com>
2017-08-28 14:29:55 +0800

    Fixed typo

--- app/controllers/mainCtrl.js
+++ app/controllers/mainCtrl.js
@@ -3,7 +3,7 @@ angular.module('mainController', [])
         let main = this;
         main.name = 'Exam Practice';
         main.author = 'Neimark Junsay Braga';
-        main.description = 'Exam Practice App is an application that would help students study for their exams. Basically, the students will encode their terminologies and meanings to dictionary section and the application will automatically will make a practice exam out of those encoded data.';
+        main.description = 'Exam Practice App is an application that would help students study for their exams. Basically, the students will encode their terminologies and meanings to dictionary section and the application will automatically make a practice exam out of those encoded data.';
         main.year = '2017';
         main.version = 'v1.0.0';
         main.navLinks = [
